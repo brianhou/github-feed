9b0105d1dfbf75b3c56e3ac0426cf8e74a63d3d4
brianhou <brian.hou@live.com>
2014-01-06 00:39:39 -0800

    Change pygithub3 to PyGithub. It works now! (But it's a bit slow...)

--- githubfeed/feed.py
+++ githubfeed/feed.py
@@ -1,45 +1,61 @@
-import pygithub3
-import requests
+import github
+import sys
 
-gh = pygithub3.Github()
+g = github.Github()
 
-def not_found(default=None):
+def not_found(default):
     def defaulted(fn):
         def protected(*args, **kwargs):
             try:
                 return fn(*args, **kwargs)
-            except pygithub3.exceptions.NotFound:
-                return default
-            except requests.exceptions.HTTPError:
-                print 'Remaining requests: %s' % gh.remaining_requests
-                exit(1)
+            except github.GithubException as e:
+                print >> sys.stderr, e
+                return default()
         return protected
     return defaulted
 
-@not_found(())
-def get_repos(username):
-    return tuple(gh.repos.list(username).iterator())
+@not_found(lambda: None)
+def get_user(username):
+    """Returns a User object or None for an invalid username."""
+    return g.get_user(username)
 
-@not_found(())
-def get_commits(username): # oops rate limiting
+@not_found(list)
+def get_repos(user):
+    """Returns a list of USER's repos."""
+    return list(user.get_repos())
+
+@not_found(list)
+def get_repo_commits(user, repo):
+    """Returns a list of the last ten commits by USER in REPO."""
+    commits = []
+    for commit in repo.get_commits():
+        if commit.author == user:
+            commits.append((commit, repo.name))
+        if len(commits) >= 10: # rate-limiting!
+            break
+    return commits
+
+def get_commits(user):
+    """Returns a list of recent commits by USER."""
     all_commits = []
-    for repo in get_repos(username):
-        print repo.name
-        repo_commits = gh.repos.commits.list(user=username, repo=repo.name)
-        my_repo_commits = filter(lambda s: s.author and s.author.login == username, repo_commits.iterator())
-        all_commits.extend(list(my_repo_commits))
+    for repo in get_repos(user):
+        all_commits.extend(get_repo_commits(user, repo))
     return all_commits
 
 def recent_commits(username, n=10):
     """Returns a list of recent commits by USERNAME. Each commit is represented
     as a Commit object."""
-    return (Commit('hi'),) * n
-    return tuple(Commit(c) for c in get_commits(username)[:n])
+    user = get_user(username)
+    if user:
+        user_commits = get_commits(user)
+        user_commits.sort(key=lambda c: c[0].commit.author.date, reverse=True)
+        return [Commit(*c) for c in user_commits[:n]]
 
 class Commit(object):
-    def __init__(self, commit_object):
-        self.message = commit_object
-        self.committer = commit_object
-        self.url = commit_object
-        self.repo = commit_object
-        self.date = commit_object
+    """Hold rendering information."""
+    def __init__(self, commit_object, repo_name):
+        self.message = commit_object.commit.message
+        self.committer = commit_object.committer.name
+        self.url = commit_object.commit.html_url
+        self.repo_name = repo_name
+        self.date = commit_object.commit.committer.date
